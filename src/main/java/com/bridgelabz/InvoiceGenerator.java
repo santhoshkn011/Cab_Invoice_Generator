d721da4c4d82aafc9135cc07189e895c2366f1b4
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-24 22:35:52 +0530

    Added: Welcome Message.

---
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class InvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cab Invoice Generator Program.");
+    }
+}
